03081f5b9332e4f9198586068b5e0c297808e5e3
Ryan Barry <ryan@nuclearice.com>
2019-02-04 00:24:40 -0800

    add some helpful scripts, install those automatically too

--- install.sh
+++ install.sh
@@ -27,3 +27,14 @@ for file in $files; do
   echo "creating symlink to $file in home directory"
   ln -fs $dir/$file ~/$file
 done
+
+# now do this for the scripts in ./bin
+scripts=$( find $dir/bin -type f -depth 1 | xargs basename )
+for script in $scripts; do
+    if [ -f ~/bin/$script ] && [ ! -L ~/bin/$script ]; then
+        mkdir -p $olddir
+        mv ~/bin/$script ~/$olddir/bin/
+    fi
+    echo "creating symlink to $script in home bin directory"
+    ln -fs $dir/bin/$script ~/bin/$script
+done
